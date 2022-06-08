c77888ff6d03dc3609ee3e6d7f04632c507c2c63
Jason Kincl <jkincl@redhat.com>
2022-06-08 14:21:51 -0400

    remove cruft

--- src/utils/navs.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import { useTranslation } from 'react-i18next';
-import { MyLandingPage } from '../components/LandingPage'
-
-export const LandingPage = () => {
-  const { t } = useTranslation('plugin__console-demo-plugin');
-  return <MyLandingPage title={t('Welcome LLNL HPC Scientists')}/>;
-};
